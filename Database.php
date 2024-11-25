b918f69b469567508d5665f738b32007ebcc6011
guih.goncalves16 <guih.goncalves16@gmail.com>
2024-11-25 11:59:07 -0300

    Att Enviroment Variables v.25.11.009

--- Database.php
+++ Database.php
@@ -1,23 +1,30 @@
 <?php
-    class Database {
-        private $host = getenv('DB_HOST');
-        private $db = getenv('DB_NAME');
-        private $user = getenv('DB_USER');
-        private $password = getenv('DB_PASSWORD');
-        public $conn;
+class Database {
+    private $host;
+    private $db;
+    private $user;
+    private $password;
+    public $conn;
 
-        public function getConnection() {
-            $this->conn = null;
-            try {
-                $this->conn = new PDO("pgsql:host={$this->host};dbname={$this->dbname}", $this->user, $this->password);
-                $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            } catch(PDOException $exception) {
-                echo "--------------------------------------------------------------------------------
--------------------------------- ERRO NA CONEXÃO -------------------------------
---------------------------------------------------------------------------------\n\n". $exception->getMessage();
-            }
-
-            return $this->conn;
-        }
+    public function __construct() {
+        $this->host = getenv('DB_HOST');
+        $this->db = getenv('DB_NAME');
+        $this->user = getenv('DB_USER');
+        $this->password = getenv('DB_PASSWORD');
     }
+
+    public function getConnection() {
+        $this->conn = null;
+        try {
+            $this->conn = new PDO("pgsql:host={$this->host};dbname={$this->db}", $this->user, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $exception) {
+            echo "--------------------------------------------------------------------------------
+-------------------------------- ERRO NA CONEXÃO -------------------------------
+--------------------------------------------------------------------------------\n\n" . $exception->getMessage();
+        }
+
+        return $this->conn;
+    }
+}
 ?>
